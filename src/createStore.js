10a7585c7ebfec4de6f17b01d92a2f22bcf648c6
Kari Narhi <Kari.Narhi24@hotmail.com>
2020-09-08 20:49:15 +0300

    started implementing Redux

---
+++ src/createStore.js
@@ -0,0 +1,14 @@
+import { createStore, applyMiddleware } from "redux";
+import ReduxThunk from "redux-thunk";
+import RootReducer from "./reducers";
+
+export const middleware = [ReduxThunk];
+
+export const createStoreWithMiddleware = applyMiddleware(...middleware)(
+  createStore
+);
+
+export const store = createStoreWithMiddleware(
+  RootReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
